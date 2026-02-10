469b6418dc0b4a99d44a7d4d649cfebe3b295393
Nom075 <graytonl@nycstudents.net>
2026-02-10 14:00:55 -0500

    wqewqewqe

---
+++ Nom/src/components/mainCookie.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <h1>OH MY DAYS</h1>
+        <img src = "/Cookie.png">
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+
+</style>
--- Nom/src/router/index.js
+++ Nom/src/router/index.js
@@ -1,8 +1,14 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import mainCookie from '@/components/mainCookie.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [{
+    path: '/',
+    name: "/",
+    component: mainCookie,
+  }
+  ],
 })
 
 export default router
