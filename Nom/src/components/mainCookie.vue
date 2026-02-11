ebefade9e10e1e83ea2d29b0e05cb6bc2148730a
Nom075 <graytonl@nycstudents.net>
2026-02-11 14:02:12 -0500

    lol!

--- Nom/src/components/mainCookie.vue
+++ Nom/src/components/mainCookie.vue
@@ -1,14 +1,52 @@
 <template>
     <div>
-        <h1>OH MY DAYS</h1>
-        <img src = "/Cookie.png">
+        <h1>Cookie Licker</h1>
+        <h2>Directions: Lick the cookie until it disappears! Buy stuff to help you.</h2>
+        <div class = "cookieScreen">
+            <button @click = "addCookie(1)"><img src = "/Cookie.png"></button>
+        </div>
+        
+        
+        <h2>Cookie crumbs: {{ crumbs }}</h2>
+        <h2>Cookie remaining: {{ totalCookie }}</h2>
     </div>
 </template>
 
 <script setup>
 
+import {ref, reactive} from 'vue';
+
+const crumbs = ref(0)
+const totalCookie = ref(1000000000)
+
+function addCookie(number){
+    crumbs.value += number
+    totalCookie.value -= number
+}
+
+
+
+
+
+
 </script>
 
 <style scoped>
-
+    h1, h2{
+        justify-self: center;
+    }
+    img{
+        height: 500px;
+        width: 500px;
+    }
+    button{
+        background: none;
+        border: none;
+    }
+    .cookieScreen{
+        flex: auto;
+        flex-direction: column;
+        justify-content: center;
+        background-color: aqua;
+    }
 </style>
--- Nom/src/router/index.js
+++ Nom/src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import mainCookie from '@/components/mainCookie.vue'
+import cookieShop from '@/components/shop.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -7,6 +8,11 @@ const router = createRouter({
     path: '/',
     name: "/",
     component: mainCookie,
+  },
+  {
+    path: '/cookieShop',
+    name: "cookieShop",
+    component: cookieShop,
   }
   ],
 })
