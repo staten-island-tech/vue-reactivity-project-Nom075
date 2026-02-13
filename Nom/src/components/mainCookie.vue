68bee4bffe901b9da69dc7a8b4bf2fcf81842fa7
Nom075 <graytonl@nycstudents.net>
2026-02-13 14:01:34 -0500

    dwfewferewrwerewr

--- Nom/src/components/mainCookie.vue
+++ Nom/src/components/mainCookie.vue
@@ -1,19 +1,18 @@
 <template>
     <div>
-        <h1 class = "justify-self-auto">Cookie Licker</h1>
-        <h2 class = "justify-self-auto">Directions: Lick the cookie until it disappears! Buy stuff to help you.</h2>
-        <div class = "flex flex-row justify-content-center">
-            <div class = "h-50% w-50%">
-                <button class = "h-500 w-500 bg-none border-none" @click = "addCookie(1)"><img src = "/Cookie.png"></button>
+        <h1 class = "justify-self-center text-5xl p-5">Cookie Licker</h1>
+        <h2 class = "justify-self-center text-2xl p-5">Directions: Lick the cookie until it disappears! Buy stuff to help you.</h2>
+        <div class = "flex flex-row justify-center w-full h-200 bg-black">
+            <div class = "flex flex-col h-full w-[40%] bg-cyan-100 justify-center items-center gap-5">
+                <h2 class = "text-4xl">Cookie</h2>
+                <h2 class = "text-2xl">Cookie crumbs: {{ crumbs }}</h2>
+                <button class = "h-[50%] w-[50%] border-none" @click = "addCookie(1)"><img src = "/Cookie.png"></button>
+                <h2 class = "text-xl">Cookie remaining: {{ totalCookie }}</h2>
             </div>
-            <div class = "h-50% w-50%">
+            <div class = "h-[50%] w-[60%]">
                 <shop></shop>
             </div>
         </div>
-        
-        
-        <h2 class = "justify-self-auto">Cookie crumbs: {{ crumbs }}</h2>
-        <h2 class = "justify-self-auto">Cookie remaining: {{ totalCookie }}</h2>
     </div>
 </template>
 
@@ -32,5 +31,5 @@ function addCookie(number){
 </script>
 
 <style scoped>
-@import "tailwindcss";
+
 </style>
--- Nom/src/components/shop.vue
+++ Nom/src/components/shop.vue
@@ -6,21 +6,6 @@
 </template>
 
 <script setup>
-import upgrade from '@/components/upgrade.vue'
-
-const upgrades = ref([
-  { name: 'Leo', species: 'Lion', weight: 420 },
-  { name: 'Ella', species: 'Elephant', weight: 5000 },
-  { name: 'Max', species: 'Dog', weight: 30 },
-  { name: 'Whiskers', species: 'Cat', weight: 4 },
-  { name: 'Bubbles', species: 'Fish', weight: 0.5 },
-  { name: 'Charlie', species: 'Horse', weight: 500 },
-  { name: 'Hoppy', species: 'Rabbit', weight: 3 },
-  { name: 'Tweety', species: 'Bird', weight: 0.2 },
-  { name: 'Gerry', species: 'Giraffe', weight: 1200 },
-  { name: 'Bella', species: 'Bear', weight: 300 },
-])
-
 
 </script>
 
